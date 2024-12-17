d7d9a4bf7a7f463d5e907d293ccd9589c1420d7d
Binuri <pererabinuri29@gmail.com>
2024-12-17 18:34:22 +0530

    testing common layout

--- src/app/layout/shared/common-layout/common-layout.component.spec.ts
+++ src/app/layout/shared/common-layout/common-layout.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommonLayoutComponent } from './common-layout.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CommonLayoutComponent', () => {
   let component: CommonLayoutComponent;
@@ -8,7 +9,7 @@ describe('CommonLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CommonLayoutComponent]
+      imports: [CommonLayoutComponent, RouterTestingModule]
     })
     .compileComponents();
 
