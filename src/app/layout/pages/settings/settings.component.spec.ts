55d2c78ac154aad37867e14ae124d57a62ed64c2
Binuri <pererabinuri29@gmail.com>
2024-12-17 12:58:48 +0530

    unit testing with karma

--- src/app/layout/pages/settings/settings.component.spec.ts
+++ src/app/layout/pages/settings/settings.component.spec.ts
@@ -2,6 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsComponent } from './settings.component';
 
+/*
+fdescribe('SettingsComponent', () => {
+  it('first test script', () => {
+    expect(10).toBe(10);
+  });
+});
+*/
+
+
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
